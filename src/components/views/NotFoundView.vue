2b537ab63b228fce48d992c2df110e7986374229
NotErisDev <windows2003server2003@gmail.com>
2026-06-24 20:57:44 +0700

    Added 404 error page

---
+++ src/components/views/NotFoundView.vue
@@ -0,0 +1,34 @@
+<script setup>
+document.title = '404';
+</script>
+
+<template>
+  <div class="container errorContainer">
+    <h1>404</h1>
+    <p>Страница не найдена</p>
+    <a href="/" class="btn btn-lg btn-primary">На главную</a>
+  </div>
+</template>
+
+<style>
+.errorContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 128px;
+  min-height: 50vh;
+}
+
+.errorContainer h1 {
+  font-size: 108px;
+}
+
+.errorContainer p {
+  color: var(--color-secondary);
+  margin-top: -16px;
+}
+
+.errorContainer .btn {
+  margin-top: 64px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import GameJoinView from "@/components/views/GameJoinView.vue";
 import ModeratorJoinView from "@/components/views/ModeratorJoinView.vue";
 import PrivacyView from "@/components/views/PrivacyView.vue";
+import NotFoundView from "@/components/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: GameJoinView },
     { path: '/moderator', component: ModeratorJoinView },
-    { path: '/privacy', component: PrivacyView }
+    { path: '/privacy', component: PrivacyView },
+    { path: '/:catchAll(.*)*', component: NotFoundView }
   ],
 })
 
